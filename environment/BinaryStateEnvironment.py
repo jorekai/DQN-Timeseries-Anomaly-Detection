250fe7a65a4f1f6698b8db63ae5861d4f0a336b9
jorekai <jorek@stud.fra-uas.de>
2020-11-12 17:19:41 +0100

    Modified magic func

--- environment/BinaryStateEnvironment.py
+++ environment/BinaryStateEnvironment.py
@@ -25,12 +25,11 @@ class BinaryStateEnvironment:
 
     def __state(self, action):
         """
-        :param timeseries:
-        :param cursor: the position where in the TimeSeries we are currently
+        :param action: Our state Value consists of the timeseries value and the previous taken action: [0.123, 1] or [0.123, 0]
         :return: The Value of the current position, states with the same value are treated the same way
         """
         state = np.asarray(
-            [np.float64(self.env.timeseries_labeled['value'][self.env.timeseries_cursor]), 1 if action == 1 else 0])
+            [self.timeseries_labeled['value'][self.timeseries_cursor], 1 if action == 1 else 0], dtype=float)
         return state
 
     def __reward(self, action):
@@ -39,34 +38,34 @@ class BinaryStateEnvironment:
         :return: action-reward value
         """
         state = self.__state(action)
-        label = self.env.timeseries_labeled['anomaly'][self.env.timeseries_cursor]
-        if state[1] == 1 and label == 1:
-            if action == 1:
+        label = self.timeseries_labeled['anomaly'][self.timeseries_cursor]
+        if state[1] == 1 and label == 1:  # previous action was "anomaly" and current label is "anomaly"
+            if action == 1:  # we did label correctly so true positive
                 return 5
-            if action == 0:
+            if action == 0:  # we did not label correctly so false negative
                 return -5
-        if state[1] == 1 and label == 0:
-            if action == 1:
+        if state[1] == 1 and label == 0:  # previous action was "anomaly" and current label is "normal"
+            if action == 1:  # we did not label correctly so false positive
                 return -1
-            if action == 0:
+            if action == 0:  # we did label correctly so true negative
                 return 1
-        if state[1] == 0 and label == 1:
-            if action == 1:
+        if state[1] == 0 and label == 1:  # previous action was "normal" and current label is "anomaly"
+            if action == 1:  # we did label correctly so true positive
                 return 5
-            if action == 0:
+            if action == 0:  # we did not label correctly so false negative
                 return -5
-        if state[1] == 0 and label == 0:
-            if action == 1:
+        if state[1] == 0 and label == 0:  # previous action was "normal" and current label is "normal"
+            if action == 1:  # we did not label correctly so false positive
                 return -1
-            if action == 0:
+            if action == 0:  # we did label correctly so true negative
                 return 1
 
     def reset(self):
         """
         Reset the current Series to the first Value.
-        :return: initial state
+        :return: initial state, starting action was 0
         """
-        self.env.timeseries_cursor = self.env.timeseries_cursor_init
+        self.env.timeseries_cursor = self.timeseries_cursor_init
         self.env.normalize_timeseries()
         self.env.done = False
         init_state = self.__state(0)
@@ -80,9 +79,9 @@ class BinaryStateEnvironment:
         """
         current_state = self.__state(action)
         reward = self.__reward(action)
-        self.env.update_cursor()
+        self.update_cursor()
         next_state = self.__state(action)
-        return current_state, action, reward, next_state, self.env.is_done(self.env.timeseries_cursor)
+        return current_state, action, reward, next_state, self.is_done()
 
     def __len__(self):
         """
@@ -101,9 +100,8 @@ class BinaryStateEnvironment:
 
 
 if __name__ == '__main__':
-    config = ConfigTimeSeries(seperator=",", window=1)
     env = BinaryStateEnvironment(
-        TimeSeriesEnvironment(verbose=True, filename="./Test/SmallData.csv", config=config, window=True))
+        TimeSeriesEnvironment(verbose=True, filename="./Test/SmallData.csv", config=ConfigTimeSeries()))
     env.reset()
     idx = 1
     while True:
